0deb4cab0f011de63f962543bdfb3119ecba865a
Julio Moreno <julioc.morenob@gmail.com>
2021-07-07 22:35:49 -0400

    Authentication in progress

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.get('/', (request, response) => {
   response.render("login");
 });
 
+require('dotenv').config();
+
 app.get('/index.html', async (request, response, next) => {
     try{
         var dbQuery = await db.query("SELECT * FROM restaurant");
@@ -31,7 +33,30 @@ app.get('/index.html', async (request, response, next) => {
     }   
 });
 
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log('Listening on port ' + PORT);
+});
+
+const { auth, requiresAuth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: process.env.baseURL,
+  clientID: process.env.clientID,
+  issuerBaseURL: process.env.issuerBaseURL,
+  secret: process.env.secret,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+});
+
+app.get('/profile',requiresAuth(), (req, res)=>{
+  res.send(JSON.stringify(req.oidc.user));
 });
